8c5783db1eea1a7194183af08c7dcca01aeb52b9
lovurself <lovurself_44@naver.com>
2022-03-07 14:16:12 +0900

    React styling - styled-components 라이브러리를 사용(기본 문법)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React, { useState } from "react";
-import CheckBox from "./components/CheckBox";
+import React from "react";
+import styled, { css } from 'styled-components';
+
+const Circle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background: ${props => props.color};
+  border-radius: 50%;
+  margin-left: 1rem;
+  margin-top: 1rem;
+  ${props => props.huge && 
+    css`
+    // props 사용가능
+    width: 10rem;
+    height: 10rem;
+  `}
+`;
 
 function App() {
-  const [check, setCheck] = useState(false);
-  // boolean타입으로 상태 관리
-  const onChange = (e) => {
-    setCheck(e.target.checked);
-  }
   return (
-    <div>
-      <CheckBox onChange={onChange} checked={check}>다음 약관에 모두 동의</CheckBox>
-    </div>
-  );
+    <>
+      <Circle color='black' />
+      <Circle color='blue' huge />
+    </>
+    
+  )
 }
 
 export default App;
