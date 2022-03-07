f7032462a0049be93e9ad6cf788376f0fd6fc7d7
lovurself <lovurself_44@naver.com>
2022-03-07 23:59:10 +0900

    React styling - polished 라이브러리를 사용하여 button 색상 관리

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,22 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { darken, lighten } from 'polished';
+
+// StyledButton 밖에서 변수 생성하고 안에 넣어주는 방법
+// const colorStyles = css`
+//     ${({ theme, color }) => {
+//         const selected = theme.palette[color];
+//         return css`
+//             background: ${selected};
+//             &:hover {
+//                 background: ${lighten(0.1, selected)};
+//             }
+//             &:active {
+//                 background: ${darken(0.1, selected)}
+//             }
+//         `
+//     }}
+// `
 
 const StyledButton = styled.button`
     /* 공통 스타일 */
@@ -18,14 +35,44 @@ const StyledButton = styled.button`
     height: 2.25rem;
     font-size: 1rem;
 
+    /* colorStyles 적용하는 방법
+    ${colorStyles}; */
+
     /* 색상 */
-    background: #228be6;
+    /* ${props => {
+        const color = props.theme.palette[props.color];
+        return css`
+            background: ${color};
+            &:hover {
+                background: ${lighten(0.1, color)};
+            }
+            &:active {
+                background: ${darken(0.1, color)}
+            }
+        `
+    }} */
+    // 비구조화 할당
+    ${({ theme, color }) => {
+        const selected = theme.palette[color];
+        return css`
+            background: ${selected};
+            &:hover {
+                background: ${lighten(0.1, selected)};
+            }
+            &:active {
+                background: ${darken(0.1, selected)}
+            }
+        `
+    }}
+
+    // ThemeProvider 사용
+    /* background: ${props => props.theme.palette.blue};
     &:hover {
-        background: #339af0;
+        background: ${props => lighten(0.1, props.theme.palette.blue)};
     }
     &:active {
-        background: #1c7ed6;
-    }
+        background: ${props => darken(0.1, props.theme.palette.blue)};
+    } */
 
     /* 기타 */
     & + & {
@@ -33,8 +80,12 @@ const StyledButton = styled.button`
     }
 `;
 
-function Button({ children, ...rest }) {
-  return <StyledButton {...rest}>{children}</StyledButton>
+function Button({ children, color, ...rest }) {
+  return <StyledButton color={color} {...rest}>{children}</StyledButton>
 };
+
+Button.defaultProps = {
+    color: 'blue'
+}
 
 export default Button;
