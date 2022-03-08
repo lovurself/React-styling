02b719f425bf54994f9709416d4e4f018a29a66a
lovurself <lovurself_44@naver.com>
2022-03-08 17:38:12 +0900

    React styling - Styled-components로 button에 outline, fullWidth 설정

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const ButtonGroup = styled.div`
   & + & {
     margin-top: 1rem;
   }
-`
+`;
 
 const palette = {
   blue: '#228be6',
@@ -28,18 +28,18 @@ function App() {
       <AppBlock>
         <ButtonGroup>
           <Button size='large'>BUTTON</Button>
-          <Button>BUTTON</Button>
-          <Button size='small'>BUTTON</Button>
-        </ButtonGroup>
-        <ButtonGroup>
-          <Button size='large' color='gray'>BUTTON</Button>
           <Button color='gray'>BUTTON</Button>
-          <Button size='small' color='gray'>BUTTON</Button>
+          <Button color='pink' size='small'>BUTTON</Button>
         </ButtonGroup>
         <ButtonGroup>
-          <Button size='large' color='pink'>BUTTON</Button>
-          <Button color='pink'>BUTTON</Button>
-          <Button size='small' color='pink'>BUTTON</Button>
+          <Button size='large' outline>BUTTON</Button>
+          <Button color='gray' outline>BUTTON</Button>
+          <Button color='pink' size='small' outline>BUTTON</Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button fullWidth>BUTTON</Button>
+          <Button color='gray' fullWidth>BUTTON</Button>
+          <Button color='pink' fullWidth>BUTTON</Button>
         </ButtonGroup>
       </AppBlock>
     </ThemeProvider>
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 import { darken, lighten } from 'polished';
 
-// StyledButton 밖에서 변수 생성하고 안에 넣어주는 방법
 const colorStyles = css`
     ${({ theme, color }) => {
         const selected = theme.palette[color];
@@ -14,6 +13,15 @@ const colorStyles = css`
             &:active {
                 background: ${darken(0.1, selected)}
             }
+            ${props => props.outline && css`
+                color: ${selected};
+                background: none;
+                border: 1px solid ${selected};
+                &:hover {
+                    background: ${selected};
+                    color: white;
+                }
+            `}
         `;
     }}
 `;
@@ -34,35 +42,26 @@ const sizes = {
 }
 
 const sizeStyles = css`
-    /* 비구조화 할당 */
     ${({ size }) => css`
         height: ${sizes[size].height};
         font-size: ${sizes[size].fontSize};
     `}
+`;
 
-    /* ${props => 
-        props.size === 'large' && css`
-            height: 3rem;
-            font-size: 1.25rem;
-        `
-    }
-    ${props => 
-        props.size === 'medium' && css`
-            height: 2.25rem;
-            font-size: 1rem;
-        `
-    } 
-    ${props => 
-        props.size === 'small' && css`
-            height: 1.75rem;
-            font-size: 0.875rem;
-        `
-    } */
+const fullWidthStyle = css`
+    ${props => props.fullWidth && css`
+        width: 100%;
+        justify-content: center;
+        & + & {
+            margin-left: 0;
+            margin-top: 1rem;
+        }
+    `}
 `;
 
 const StyledButton = styled.button`
     /* 공통 스타일 */
-    display: inline-flex;
+    display: inline-block;
     outline: none;
     border: none;
     border-radius: 4px;
@@ -71,75 +70,18 @@ const StyledButton = styled.button`
     cursor: pointer;
     padding-left: 1rem;
     padding-right: 1rem;
-    align-items: center;
 
-    /* 크기 */
-    /* ${props => 
-        props.size === 'large' && css`
-            height: 3rem;
-            font-size: 1.25rem;
-        `
-    }
-    ${props => 
-        props.size === 'medium' && css`
-            height: 2.25rem;
-            font-size: 1rem;
-        `
-    } 
-    ${props => 
-        props.size === 'small' && css`
-            height: 1.75rem;
-            font-size: 0.875rem;
-        `
-    } */
-
-    ${colorStyles}
-    ${sizeStyles}
-
-    /* 색상 */
-    /* ${props => {
-        const color = props.theme.palette[props.color];
-        return css`
-            background: ${color};
-            &:hover {
-                background: ${lighten(0.1, color)};
-            }
-            &:active {
-                background: ${darken(0.1, color)}
-            }
-        `
-    }} */
-    // 비구조화 할당
-    /* ${({ theme, color }) => {
-        const selected = theme.palette[color];
-        return css`
-            background: ${selected};
-            &:hover {
-                background: ${lighten(0.1, selected)};
-            }
-            &:active {
-                background: ${darken(0.1, selected)}
-            }
-        `
-    }} */
-
-    // ThemeProvider 사용
-    /* background: ${props => props.theme.palette.blue};
-    &:hover {
-        background: ${props => lighten(0.1, props.theme.palette.blue)};
-    }
-    &:active {
-        background: ${props => darken(0.1, props.theme.palette.blue)};
-    } */
-
-    /* 기타 */
     & + & {
         margin-left: 1rem;
     }
+
+    ${colorStyles}
+    ${sizeStyles}
+    ${fullWidthStyle}
 `;
 
-function Button({ children, color, size, ...rest }) {
-  return <StyledButton color={color} size={size} {...rest}>{children}</StyledButton>
+function Button({ children, color, size, outline, fullWidth, ...rest }) {
+  return <StyledButton color={color} size={size} outline={outline} fullWidth={fullWidth} {...rest}>{children}</StyledButton>
 };
 
 Button.defaultProps = {
